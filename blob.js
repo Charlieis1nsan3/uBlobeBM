d31b13f8a2453e246bc05a1812ec8d30fb8e0cb1
HyperNut <charliemorrison960@gmail.com>
2024-11-13 17:11:38 -0500

    Update blob.js

--- blob.js
+++ blob.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     if (window.location.href !== 'https://charlieisinsane.neocities.org/Hack.html') {
         var script = document.createElement('script');
-        script.src = 'https://blobby-boi.github.io/uBlobeBM/main.js';
+        script.src = 'https://github.com/Charlieis1nsan3/uBlobeBM/blob/main/blob.js';
         document.body.appendChild(script);
     }
 });
